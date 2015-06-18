1b6ff06f8b3c80ecb9400c14699dcb218b489556
Christophe Jollivet <christophe.jollivet@gmail.com>
2015-06-18 15:23:03 +0200

    shared Preference

--- TPAsycn/app/src/main/java/fr/apside/tpasycn/MainActivity.java
+++ TPAsycn/app/src/main/java/fr/apside/tpasycn/MainActivity.java
@@ -5,26 +5,31 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     private Button button;
+    private Button switchPref;
     private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i("MY_APP", "#############Here we go");
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +38,21 @@ public class MainActivity extends Activity {
             }
         });
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        switchPref = (Button) findViewById(R.id.button2);
+        switchPref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                boolean val_boolean = sharedPreferences.getBoolean("key_1", true);
+                Log.i("MY_APP", "#############value is " + val_boolean);
+                SharedPreferences.Editor edit = sharedPreferences.edit();
+                edit.putBoolean("key_1", !val_boolean);
+                edit.commit();
+            }
+        });
+
+
     }
 
 
@@ -59,6 +79,11 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Toast.makeText(this, "change on key " + key, Toast.LENGTH_LONG).show();
+    }
+
     class MyDownloadTask extends AsyncTask<String, Integer, String> {
 
         @Override
@@ -79,16 +104,16 @@ public class MainActivity extends Activity {
             PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 1, i, PendingIntent.FLAG_ONE_SHOT);
 
             Notification.Builder notifBuilder = new Notification.Builder(MainActivity.this)
-                            .setSmallIcon(R.drawable.ic_launcher)
-                            .setContentTitle("Un content title")
-                            .setTicker("un Ticker")
-                            .setContentText("un content texte")
-                            .setAutoCancel(true)
-                            .setVibrate(new long[]{0,500, 500, 500})
-                            .setLights(0xff0000ff, 500, 500)
-                            .setContentIntent(pendingIntent);
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentTitle("Un content title")
+                    .setTicker("un Ticker")
+                    .setContentText("un content texte")
+                    .setAutoCancel(true)
+                    .setVibrate(new long[]{0, 500, 500, 500})
+                    .setLights(0xff0000ff, 500, 500)
+                    .setContentIntent(pendingIntent);
 
-            ((NotificationManager)getSystemService(NOTIFICATION_SERVICE)).notify(2, notifBuilder.build());
+            ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(2, notifBuilder.build());
         }
 
         @Override
@@ -99,13 +124,13 @@ public class MainActivity extends Activity {
 
         @Override
         protected String doInBackground(String... params) {
-            for(int i =0; i<10; i++) {
+            for (int i = 0; i < 10; i++) {
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                publishProgress(i+1);
+                publishProgress(i + 1);
             }
             return null;
         }
